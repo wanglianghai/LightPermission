b360de3957fd8ba0e1562276ed6ed748968f810c
wanglianghai <1540446221@qq.com>
2017-08-12 14:04:08 +0800

    origin

---
+++ app/src/main/java/com/bignerdranch/android/lightpermission/MainActivity.java
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.lightpermission;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int PERMISSIONS_REQUEST_CALL_PHONE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            PERMISSIONS_REQUEST_CALL_PHONE);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "10086"));
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
